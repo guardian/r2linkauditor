ab7cedfdccf121cfa2577bdfb99d29c0753ea7c1
Ken Lim <kenneth.lim@guardian.co.uk>
2012-11-16 12:44:58 +0000

    Reports are saved in a time stamped folder.

--- build.sbt
+++ build.sbt
@@ -5,3 +5,11 @@ version := "1.0"
 scalaVersion := "2.9.1"
 
 libraryDependencies += "org.jsoup" % "jsoup" % "1.7.1"
+
+libraryDependencies += "com.github.scala-incubator.io" %% "scala-io-core" % "0.4.1-seq"
+
+libraryDependencies += "com.github.scala-incubator.io" %% "scala-io-file" % "0.4.1-seq"
+
+libraryDependencies += "joda-time" % "joda-time" % "2.1"
+
+libraryDependencies += "org.joda" % "joda-convert" % "1.1"
--- src/main/scala/com/gu/LinkAuditor/CountingSpider.scala
+++ src/main/scala/com/gu/LinkAuditor/CountingSpider.scala
@@ -5,11 +5,19 @@ import org.jsoup.nodes._
 import collection.JavaConversions._
 import java.net.URL
 import collection.mutable.HashSet
+import scalax.file.Path
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 
 object CountingSpider extends App {
 
   val r1History = new HashSet[String]
   val visitedLinks = new HashSet[String]
+
+  val dir = "links-%s".format(DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm").print(new DateTime))
+  val linkComparisonFile = Path(dir, "linkComparison")
+  val r1LinksFile = Path(dir, "r1Links")
+  val brokenLinksFile = Path(dir, "404s")
 
   def getUrlAndRecurse(currentUrl: String, recursionDepth: Int, previousUrl: String) {
     if (recursionDepth >= 0 && !visitedLinks.contains(currentUrl)) {
@@ -26,26 +34,30 @@ object CountingSpider extends App {
         val relativeLinks = linkList.filter(_.startsWith("/"))
         val r1Links = internalLinks.filter(_.contains(",,"))
 
-        println("%s| %s| %s".format(currentUrl, internalLinks.length, relativeLinks.length))
+        linkComparisonFile.append("%s| %s| %s\n".format(currentUrl, internalLinks.length, relativeLinks.length))
 
-        r1Links filterNot (r1History.contains(_)) foreach{ r1Link =>
-          r1History add r1Link
-          println("%s contains r1 link: %s".format(currentUrl, r1Link))
+        r1Links filterNot (r1History.contains(_)) foreach {
+          r1Link =>
+            r1History add r1Link
+            r1LinksFile.append("%s contains r1 link: %s\n".format(currentUrl, r1Link))
         }
 
-        visitedLinks add currentUrl   // add existing page to the "done" list.
-        
-        internalLinks foreach { nextUrl =>
-          getUrlAndRecurse(nextUrl, recursionDepth - 1, currentUrl)
+        visitedLinks add currentUrl // add existing page to the "done" list.
+
+        internalLinks foreach {
+          nextUrl =>
+            getUrlAndRecurse(nextUrl, recursionDepth - 1, currentUrl)
         }
 
       } catch {
         case ex: java.net.SocketTimeoutException => println("Timed out getting %s".format(currentUrl))
-        case ex: org.jsoup.HttpStatusException => println("%s contains broken link: %s".format(previousUrl, currentUrl))
+        case ex: org.jsoup.HttpStatusException => {
+          brokenLinksFile.append("Exception getting %s from %s\n".format(currentUrl, previousUrl))
+        }
       }
     }
-    }
-
-    println("Url; Absolute internal link count; Relative link count")
-    getUrlAndRecurse("http://www.guardian.co.uk", 2, "Start of job")
   }
+
+  println("Url; Absolute internal link count; Relative link count")
+  getUrlAndRecurse("http://www.guardian.co.uk", 2, "Start of job")
+}
