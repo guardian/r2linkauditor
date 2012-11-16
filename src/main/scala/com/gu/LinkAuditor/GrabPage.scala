820270599b6174c58765aa491bc171ba0c0306b7
Kelvin Chappell <kelvin.chappell@guardian.co.uk>
2012-11-16 11:23:37 +0000

    Added method to show outbound links from a single URL

--- src/main/scala/com/gu/LinkAuditor/GrabPage.scala
+++ src/main/scala/com/gu/LinkAuditor/GrabPage.scala
@@ -1,9 +1,9 @@
 package com.gu.LinkAuditor
 
 import org.jsoup.Jsoup
-import org.jsoup.nodes._
-import collection.JavaConversions
+import collection.JavaConversions._
 import java.net.URL
+import org.jsoup.nodes.Element
 
 object GrabPage extends App {
 
@@ -15,10 +15,9 @@ object GrabPage extends App {
       try {
         val doc = Jsoup.connect(target.toString).get
 
-        val links: Iterator[Element] = JavaConversions.asScalaIterator(doc.select("a[href]").listIterator())
-        val linkList = links.toList.map(_.attr("href")).filterNot(_.startsWith("#")) // don't want fragment urls.
-        val internalLinks = linkList.filter(_.startsWith("http://%s/".format(targetHost)))
-        val relativeLinks = linkList.filter(_.startsWith("/"))
+        val links = doc.select("a[href]").toList.map(_.attr("href")).filterNot(_.startsWith("#")) // don't want fragment urls.
+        val internalLinks = links.filter(_.startsWith("http://%s/".format(targetHost)))
+        val relativeLinks = links.filter(_.startsWith("/"))
 
         println("%s; %s; %s".format(url, internalLinks.length, relativeLinks.length))
         internalLinks foreach {
@@ -28,8 +27,48 @@ object GrabPage extends App {
         case ex: java.net.SocketTimeoutException => println("Timed out getting %s".format(url))
       }
     }
+  }
+
+  def reportOutboundLinks(url: String) {
+
+    def report(links: List[Element]) {
+      links.foreach(link => println("%s:\n%s\n".format(link.text(), link.attr("href"))))
     }
 
-    println("Url; Absolute internal link count; Relative link count")
-    getUrlAndRecurse("http://www.guardian.co.uk", 2)
+    val target = new URL(url)
+    val targetHost = target.getHost
+
+    try {
+      val doc = Jsoup.connect(target.toString).get
+
+      val links = doc.select("a[href]").toList.filterNot(_.attr("href").startsWith("#")) // don't want fragment urls.
+      val internalAbsLinks =
+        links.filter(_.attr("href").startsWith("http://%s/".format(targetHost)))
+          .sortBy(_.attr("href"))
+      val domainRelLinkElements = links.filter(_.attr("href").startsWith("/"))
+        .sortBy(_.attr("href"))
+      val externalLinks =
+        (links.toSet &~ internalAbsLinks.toSet &~ domainRelLinkElements.toSet).toList
+          .sortBy(_.attr("href"))
+
+      println("+++++ INTERNAL ABS +++++")
+      println()
+      report(internalAbsLinks)
+
+      println("+++++ DOMAIN REL +++++")
+      println()
+      report(domainRelLinkElements)
+
+      println("+++++ EXTERNAL +++++")
+      println()
+      report(externalLinks)
+
+    } catch {
+      case ex: java.net.SocketTimeoutException => println("Timed out getting %s".format(url))
+      case other: Exception => println(other.getMessage)
+    }
   }
+
+  println("Url; Absolute internal link count; Relative link count")
+  getUrlAndRecurse("http://www.guardian.co.uk", 2)
+}
